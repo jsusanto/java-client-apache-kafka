84174931243cc4d0c52ebc67704d17cdc99edc53
Jeffry Susanto <jeffry.susanto@medibank.com.au>
2023-10-06 13:26:41 +1100

    Add HelloWorld to test Maven project and ProducerDemo

---
+++ src/main/java/com/medibank/javaclientkafka/HelloWorld.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.medibank.javaclientkafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author 606041
+ */
+public class HelloWorld {
+    private static final Logger log = LoggerFactory.getLogger(HelloWorld.class);
+
+    public static void main(String[] args) {
+        log.info("Hello World");
+    }
+}
---
+++ src/main/java/com/medibank/javaclientkafka/ProducerDemo.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.medibank.javaclientkafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+/**
+ *
+ * @author 606041
+ */
+public class ProducerDemo {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class);
+
+    public static void main(String[] args) {
+        log.info("I am a Kafka Producer");
+
+        String bootstrapServers = "13.238.116.115:9092";
+
+        // create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // create a producer record
+        ProducerRecord<String, String> producerRecord =
+                new ProducerRecord<>("demo_java", "hello world");
+
+        // send data - asynchronous
+        producer.send(producerRecord);
+
+        // flush data - synchronous
+        producer.flush();
+        // flush and close producer
+        producer.close();
+    }
+}
